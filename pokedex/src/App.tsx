ed4ba38c1df513e32cb26459a0039cf03af4d856
markelgomez-coder <markel.gomezr@gmail.com>
2026-05-14 11:12:12 +0200

    Import: He importado el index y va bien por ahora

--- pokedex/src/App.tsx
+++ pokedex/src/App.tsx
@@ -1,122 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import "./css/variables.css";
+import "./css/static.css";
+import "./css/iconos.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+      <header>
+        <div id="header-icono">
+          <div className="pokeball-icono">
+            <div className="pokeball-icono-interior"></div>
+            <div className="pokeball-icono-vector1"></div>
+            <div className="pokeball-icono-vector2"></div>
+            <div className="pokeball-icono-vector3"></div>
+            <div className="pokeball-icono-vector4"></div>
+            <div className="pokeball-icono-vector5"></div>
+          </div>
+          <h1>Pokedex</h1>
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+        <nav>
+          <a href="index.html">Home</a>
+          <a href="pokedex.html">Pokedex</a>
+          <a href="dream-team.html">Dream Team</a>
+        </nav>
+      </header>
+      <main>
+        <h2>Pokemon</h2>
+        <p>
+          Welcome to the Pokedex! This is your ultimate guide to all things
+          Pokémon. Here, you can explore detailed information about every
+          Pokémon species, including their types, abilities, evolutions, and
+          much more. Whether you're a seasoned trainer or just starting your
+          Pokémon journey, the Pokédex is your go-to resource for learning about
+          the fascinating world of Pokémon. Dive in and discover the unique
+          characteristics of each Pokémon, their habitats, and how they interact
+          with one another. Happy exploring!
+        </p>
+      </main>
+      <footer>
+        <p>© 2026 Markel Gomez. All rights reserved.</p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
