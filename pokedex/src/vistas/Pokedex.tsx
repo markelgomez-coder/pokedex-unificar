d4820bb7d06c9cd7243a4cf80eaaafa8027caab0
markelgomez-coder <markel.gomezr@gmail.com>
2026-05-20 12:40:20 +0200

    Component: Mejorado el componente pokedex, ahora enseña cartas bien y el input funciona (el buscador)

--- pokedex/src/vistas/Pokedex.tsx
+++ pokedex/src/vistas/Pokedex.tsx
@@ -1,43 +1,73 @@
 import "../css/pokedex.css";
 import "../css/variables.css";
 import "../css/static.css";
+import * as funcionesAPI from "../js/src/ts/funciones-API";
 import CartaPokemonVacia from "../componentes/CartaPokemonVacia";
 import CartaPokemon from "../componentes/CartaPokemon";
-
+import { useEffect, useState } from "react";
+import type { Pokemon } from "../ts/tipos";
 
 function Pokedex() {
-  const pokemon = {
-    numero: 1,
-    nombre: "Bulbasaur",
-    tipos: ["grass", "poison"],
-    imagen: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
-    peso: 6.9,
-    altura: 0.7,
-    hp: 45,
-    atk: 49,
-    def: 49,
-    sat: 65,
-    sdf: 65,
-    spd: 45,
-  };
+
+  const [pokemon, setPokemon] = useState<Pokemon | null>(null);
+  const [busqueda, setBusqueda] = useState("");
+
+  useEffect(() => {
+    async function cargarPokemon() {
+      const data = await funcionesAPI.obtenerPokemon(1);
+      setPokemon(data);
+    }
+
+    cargarPokemon();
+  }, []);
+
+  useEffect(() => {
+
+    const timeout = setTimeout(async () => {
+
+      if (busqueda.trim() !== "") {
+        const data = await funcionesAPI.obtenerPokemon(busqueda);
+        setPokemon(data);
+      }
+
+    }, 500);
+
+    return () => clearTimeout(timeout);
+
+  }, [busqueda]);
+
   return (
     <>
       <div className="buscador">
+
         <div className="lupa-icono">
           <div className="lupa-icono-circulo"></div>
           <div className="lupa-icono-linea"></div>
         </div>
+
         <form id="formulario-busqueda">
           <input
             type="text"
             id="input-busqueda"
             placeholder="Search Pokémon..."
+            value={busqueda}
+            onChange={(e) => setBusqueda(e.target.value)}
           />
         </form>
+
       </div>
+
       <section id="resultado-busqueda">
+
         <CartaPokemonVacia />
-        <CartaPokemon pokemon={pokemon} dreamTeam={true} />
+
+        {pokemon && (
+          <CartaPokemon
+            pokemon={pokemon}
+            dreamTeam={true}
+          />
+        )}
+
       </section>
     </>
   );
