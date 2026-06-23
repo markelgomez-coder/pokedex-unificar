6ed95be9b48257c51ff49c2aef38b4a5bf9a8c4a
markelgomez-coder <markel.gomezr@gmail.com>
2026-06-23 12:14:29 +0200

    Types: He creado nuevos tipos para poder acceder de manera ordenada a la informacion necesaria para el apartado combate

--- pokedex/src/domain/entities/pokemon.ts
+++ pokedex/src/domain/entities/pokemon.ts
@@ -57,3 +57,112 @@ export type FlavorTextEntry = {
     url: string;
   };
 };
+
+export type MovimientoCombate = {
+  nombre: string;
+
+  potencia: number | null;
+  precision: number | null;
+
+  ppMax: number;
+  ppActual: number;
+
+  prioridad: number;
+
+  tipo: string;
+
+  categoria: "physical" | "special" | "status";
+
+  estado: string | null;
+
+  probabilidadEstado: number | null;
+
+  cambiosStats: {
+    stat: string;
+    cambio: number;
+  }[];
+};
+
+export type EstadoCombate =
+  | "burn"
+  | "poison"
+  | "sleep"
+  | "paralysis"
+  | "freeze"
+  | "confusion"
+  | null;
+
+export type PokemonCombate = {
+  pokemon: Pokemon;
+
+  hpActual: number;
+
+  estado: EstadoActivo | null;
+
+  atkStage: number;
+  defStage: number;
+
+  satStage: number;
+  sdfStage: number;
+
+  spdStage: number;
+
+  accuracyStage: number;
+  evasionStage: number;
+
+  movimientos: MovimientoCombate[];
+};
+
+export type MovimientoAPI = {
+  move: {
+    name: string;
+    url: string;
+  };
+};
+
+export type StatChangeAPI = {
+  change: number;
+
+  stat: {
+    name: string;
+    url: string;
+  };
+};
+
+export type MovimientoDetalleAPI = {
+  name: string;
+
+  power: number | null;
+
+  accuracy: number | null;
+
+  pp: number;
+
+  priority: number;
+
+  damage_class: {
+    name: "physical" | "special" | "status";
+  };
+
+  type: {
+    name: string;
+  };
+
+  effect_chance: number | null;
+
+  stat_changes: StatChangeAPI[];
+
+  meta?: {
+    ailment?: {
+      name: string;
+    };
+  };
+};
+
+export type EstadoActivo = {
+  tipo: EstadoCombate;
+
+  turnosRestantes: number | null;
+
+  intensidad?: number;
+};
